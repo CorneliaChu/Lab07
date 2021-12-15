288ca4a1f486475efbb5ec84bf15bddd1df93c8c
CorneliaChu <zchu26@wisc.edu>
2021-12-14 20:57:04 -0600

    Update mapper_max.py

---
+++ mapper_max.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+def mapper():
+    for line in sys.stdin:
+        data = line.strip().split(",")
+        count = 0
+        for i in data:
+            count += 1
+        if count < 4:
+            continue
+        if count > 4:
+            data = data[0:4]
+        timestamp, city, item, cost = data
+        print(city + "," + item + "," + cost)
+
+if __name__ == "__main__":
+    # what function should run when python mapper.py is called?
+    mapper()
